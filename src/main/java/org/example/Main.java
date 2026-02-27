8f2e348c097d948da4641b364595fff5b59fb3f7
brunswick02 <romangorokhov2003@gmail.com>
2026-02-27 22:27:40 +0300

    Hello World

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        HelloWorld();
+    }
 
+    public static void HelloWorld() {
+        System.out.println("Hello world");
     }
 }
